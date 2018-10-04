a76a085806ee7d5d94491b78dd5f08a1ff8d18e5
Akshay <akshay.patil@niyuj.com>
2018-10-04 15:34:32 +0530

    readme, package file updates

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # ember-time-picker
-ember-time-picker
+
+This is an Ember addon that uses the wonderful [ember-power-select](https://github.com/cibernox/ember-power-select) component for allowing the user to select a time [am / pm] from a picklist.
+
+## Installation
+
+Ember Time Picker has the same requirements as ember-power-select.
+This is an ember-cli addon so this will install the addon:
+```
+ember install ember-time-picker
+```
+## Example #1
+
+Following is basic example with selected and disabled proporty.
+
+```handlebars
+    {{ember-time-picker
+        selected = selectedTime
+        disabled=false
+    }}
+```
+
+## Example #2
+
+Following is extended example with two timepickers. This is to handle "From Time" and "To time".
+
+```handlebars
+    From Time
+    {{ember-time-picker
+        selected = fromTime
+        disabled=false
+    }}
+    To Time
+    {{ember-time-picker
+        selected = toTime
+        startTime = fromTime
+        disabled=false
+    }}
+```
+
+## Attributes
+
+| attribute | Type | Description |
+| --- | --- | --- |
+| selected | Required | The selected option |
+| startTime | optional | This will the time from which user want to display time-list |
+| disabled | `boolean` | Default it's `false`. When truthy the component cannot be interacted |
--- addon/components/ember-time-picker.js
+++ addon/components/ember-time-picker.js
@@ -1,50 +1,63 @@
 import Component from '@ember/component';
 import { observer } from '@ember/object';
+import { warn } from '@ember/debug';
 import layout from '../templates/components/ember-time-picker';
 
+const _0 = 0,
+    _10 = 10,
+    _1440 = 1440,
+    _60 = 60,
+    _12 = 12,
+    _15 = 15,
+    _24 = 24;
+
 export default Component.extend({
-  layout,
-  init() {
-      this._super(...arguments);
-      this.set('options', []);
-      if(!this.get('interval')){
-          this.set('interval', 15)
-      }
-      this.set('isVisible', true);
-      if(this.get('startTime')){
-          this.set('isVisible', false);
-      }
-      this.populate();
-  },
-  startTimeObserver: observer('startTime', function() {
-      this.set('isVisible', false);
-      this.populate();
-  }),
-  populate() {
-      let hours, minutes, ampm;
-      let options = [];
-      for(let i = 0; i < 1440; i += this.get('interval')){
-          hours = Math.floor(i / 60);
-          minutes = i % 60;
-          if (minutes < 10){
-              minutes = '0' + minutes; // adding leading zero
-          }
-          ampm = hours % 24 < 12 ? 'AM' : 'PM';
-          hours = hours % 12;
-          if (hours === 0){
-              hours = 12;
-          }
-          if(this.get('startTime') && !this.get('isVisible')){
-              if(this.get('startTime') === `${hours}:${minutes} ${ampm}`){
-                  this.set('isVisible', true);
-              }
-          }else{
-              options.push(`${hours}:${minutes} ${ampm}`);
-          }
-      }
-      if(this.get('selected') && !options.includes(this.get('selected'))){
-          this.set('selected', '');
-      }
-      this.set('options', options);
-  }
+    layout,
+    init() {
+        this._super(...arguments);
+        if (!this.get('selected')){
+            warn('"selected" must be passed into the ember-time-picker component');
+            return null;
+        }
+        this.set('options', []);
+        if(!this.get('interval')){
+            this.set('interval', _15)
+        }
+        this.set('isVisible', true);
+        if(this.get('startTime')){
+            this.set('isVisible', false);
+        }
+        this.populate();
+    },
+    startTimeObserver: observer('startTime', function() {
+        this.set('isVisible', false);
+        this.populate();
+    }),
+    populate() {
+        let hours, minutes, ampm;
+        let options = [];
+        for(let i = _0; i < _1440; i += this.get('interval')){
+            hours = Math.floor(i / _60);
+            minutes = i % _60;
+            if (minutes < _10){
+                minutes = '0' + minutes; // adding leading zero
+            }
+            ampm = hours % _24 < _12 ? 'AM' : 'PM';
+            hours = hours % _12;
+            if (hours === _0){
+                hours = _12;
+            }
+            if(this.get('startTime') && !this.get('isVisible')){
+                if(this.get('startTime') === `${hours}:${minutes} ${ampm}`){
+                    this.set('isVisible', true);
+                }
+            }else{
+                options.push(`${hours}:${minutes} ${ampm}`);
+            }
+        }
+        if(this.get('selected') && !options.includes(this.get('selected'))){
+            this.set('selected', '');
+        }
+        this.set('options', options);
+    }
 });
